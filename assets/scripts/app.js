ff2bfdf08e93c58360a6fb114594d5b2ce296f1f
sookim-Boston <soojeong914@gmail.com>
2019-10-21 10:18:36 -0400

    Displaying images

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,8 @@
 
 const imageUploadEvents = require('./image-upload/events.js')
 const authEvents = require('./auth/events.js')
+require('./../../node_modules/packery/dist/packery.pkgd.min.js')
+require('./../../node_modules/packery/dist/packery.pkgd.js')
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
@@ -16,6 +18,10 @@ $(() => {
   $('#sign-in-form').on('submit', authEvents.onSignIn)
   $('#reset-pw-form').on('submit', authEvents.onChangePassword)
   $('#sign-out-button').on('click', authEvents.onSignOut)
+  $('.get-files').packery({
+    itemSelector: '.picture-item',
+    gutter: 10
+  })
   // annonymous function for image id retrieval through handlebars
   $('.get-files').on('click', '.get-photo', function () {
     const myImageId = $(this).data('id')
